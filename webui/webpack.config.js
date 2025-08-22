959a701d439d8dd3e32357cd2e37259e3c04eda3
heretrix <>
2025-08-22 13:04:02 +0300

    Fix issue with webpack configuration

--- webui/webpack.config.js
+++ webui/webpack.config.js
@@ -2,64 +2,66 @@ const path = require("path");
 const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-const isDev = process.env.NODE_ENV !== "production";
+module.exports = (env, argv) => {
+  const isDev = env.production != true;
 
-module.exports = {
-  entry: "./src/index.tsx",
-  devtool: isDev ? "inline-source-map" : false,
-  mode: isDev ? "development" : "production",
-  output: {
-    path: path.resolve(__dirname, "./static/site"),
-    filename: isDev ? "bundle.js" : "[name].[contenthash].js",
-    publicPath: "/static/site/",
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: "ts-loader",
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/,
-        loader: "file-loader",
-        options: {
-          outputPath: "../fonts",
-        },
-      },
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
-      },
-    ],
-  },
-  resolve: {
-    extensions: [".tsx", ".ts", ".js"],
-  },
-  optimization: {
-    minimize: !isDev,
-    splitChunks: isDev ? false : {
-      chunks: "all",
-      cacheGroups: {
-        vendor: {
-          test: /[\\/]node_modules[\\/]/,
-          name(module) {
-            const pkg = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
-            return `vendor.${pkg.replace('@', '')}`;
-          },
-          chunks: "all",
-          enforce: true,
-        },
-      },
+  return {
+    entry: "./src/index.tsx",
+    devtool: isDev ? "inline-source-map" : false,
+    mode: isDev ? "development" : "production",
+    output: {
+      path: path.resolve(__dirname, "./static/site"),
+      filename: isDev ? "bundle.js" : "[name].[contenthash].js",
+      publicPath: "/static/site/",
     },
-    runtimeChunk: isDev ? false : "single",
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "public/index.html",
-      filename: path.resolve(__dirname, "templates/site/index.html"),
-    }),
-    new webpack.HotModuleReplacementPlugin(), // у деві
-  ],
-  watch: isDev,
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: "ts-loader",
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.(woff|woff2|eot|ttf|otf)$/,
+          loader: "file-loader",
+          options: {
+            outputPath: "../fonts",
+          },
+        },
+        {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"],
+        },
+      ],
+    },
+    resolve: {
+      extensions: [".tsx", ".ts", ".js"],
+    },
+    optimization: {
+      minimize: !isDev,
+      splitChunks: isDev ? false : {
+        chunks: "all",
+        cacheGroups: {
+          vendor: {
+            test: /[\\/]node_modules[\\/]/,
+            name(module) {
+              const pkg = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
+              return `vendor.${pkg.replace('@', '')}`;
+            },
+            chunks: "all",
+            enforce: true,
+          },
+        },
+      },
+      runtimeChunk: isDev ? false : "single",
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: "public/index.html",
+        filename: path.resolve(__dirname, "templates/site/index.html"),
+      }),
+      new webpack.HotModuleReplacementPlugin(),
+    ],
+    watch: isDev,
+  }
 };
