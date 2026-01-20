da7e6f9eb63ca6e024f915fd7aef09b362561607
heretrix <>
2026-01-20 17:28:30 +0200

    Fix issue with env

--- repository/app_env.py
+++ repository/app_env.py
@@ -5,6 +5,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 env_path = BASE_DIR / ".env"
+use_env = False
 if os.path.isfile(env_path):
     env = environ.Env(DEBUG=(bool, False))
     env.read_env(str(env_path), overwrite=True)
