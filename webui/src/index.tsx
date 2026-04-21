7645dd1c28a3e0a8b1c93ed1ddc1f4c863024ffc
HereTrix <shadowcodermail@gmail.com>
2026-04-21 12:38:12 +0300

    Add dark theme (#11)
    Co-authored-by: heretrix <>

--- webui/src/index.tsx
+++ webui/src/index.tsx
@@ -2,6 +2,9 @@ import React from "react"
 import App from "./components/App"
 import { createRoot } from "react-dom/client";
 
+const savedTheme = localStorage.getItem('theme') ?? 'light'
+document.documentElement.setAttribute('data-bs-theme', savedTheme)
+
 const root = createRoot(document.getElementById("app") as HTMLElement);
 root.render(
     <App />
