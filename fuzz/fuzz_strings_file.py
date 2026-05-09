a83dc26a9a9f03b67c93fbade9b9d443ba60d653
heretrix <>
2026-05-09 11:41:18 +0300

    Fix fuzz issues
    Signed-off-by: heretrix <>

--- fuzz/fuzz_strings_file.py
+++ fuzz/fuzz_strings_file.py
@@ -5,6 +5,7 @@ import io
 import os
 import sys
 
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fuzz.fuzz_settings')
 import django
 django.setup()
--- fuzz/fuzz_xcstrings_file.py
+++ fuzz/fuzz_xcstrings_file.py
@@ -6,6 +6,7 @@ import json
 import os
 import sys
 
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fuzz.fuzz_settings')
 import django
 django.setup()
